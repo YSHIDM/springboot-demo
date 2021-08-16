9a724ea896533a68edf80daf5e48be2819bbbbd8
YSHIDM <1179082420@qq.com>
2021-08-16 11:54:44 +0800

    更新

--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
 # demo
 
 springboot 尝试
+
+`http://localhost:8080/`
+
+`http://localhost:8080/showCities`
--- src/test/java/com/yshidm/demo/UnitTest.java
+++ src/test/java/com/yshidm/demo/UnitTest.java
@@ -3,32 +3,36 @@ package com.yshidm.demo;
 import static org.junit.Assert.*;
 // import static org.hamcrest.Matchers.*;
 
-import com.yshidm.demo.model.Todo;
-import com.yshidm.demo.service.ITodoService;
+// import com.yshidm.demo.model.Todo;
+// import com.yshidm.demo.service.ITodoService;
+// import org.springframework.beans.factory.annotation.Autowired;
 
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 
+import lombok.extern.slf4j.Slf4j;
+@Slf4j
 public class UnitTest {
 
-  @Autowired
-  private ITodoService todoService;
+  // @Autowired
+  // private ITodoService todoService;
 
   @Test
   public void testA() {
-    // var todo = new Todo("测试", "plan", "内容");
-    var todo = new Todo("TODO001", "测试", "plan", "内容", false, false);
-    System.out.println(todo);
-    todo = todoService.saveTodo(todo);
-    System.out.println(todo);
+        // var todo = new Todo("测试", "plan", "内容");
+        // var todo = new Todo("TODO001", "测试", "plan", "内容", false, false);
+        // System.out.println(todo);
+        // todo = todoService.saveTodo(todo);
+        // System.out.println(todo);
 
-    var t = todoService.findAll();
+        // var t = todoService.findAll();
 
-    System.out.println(t.toString());
+        // System.out.println(t.toString());
 
     var words = "Hello World";
     var hello = new String(words);
 
+    log.info("[比较]: {} 和 {}", words, hello);
+    System.out.println("汉字 111");
     assertEquals(words, hello);
   }
 }
